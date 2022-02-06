dcde13cb90eb8d45b67938843668380acfca864c
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-06 16:39:15 -0300

    feat: create product router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { CreateProductsController } from './controllers/CreateProductsController';
 import { ListProductsController } from './controllers/ListProductsController';
 
 const routes = Router();
 
-routes.get('/products', new ListProductsController().handler);
+routes.get('/products', new ListProductsController().handle);
+routes.post('/products', new CreateProductsController().handle);
 
 export { routes };
