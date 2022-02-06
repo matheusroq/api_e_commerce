d6b72f3d76b70efdf4cc82781819ca7c8179eeed
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-06 20:02:52 -0300

    feat: create product image route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CreateProductsController } from './controllers/CreateProductsController';
+import { CreateProductsImagesController } from './controllers/CreateProductsImagesController';
 import { ListProductsController } from './controllers/ListProductsController';
 
 const routes = Router();
 
 routes.get('/products', new ListProductsController().handle);
 routes.post('/products', new CreateProductsController().handle);
+routes.post('/images', new CreateProductsImagesController().handle);
 
 export { routes };
