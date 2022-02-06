6e50a4989c3ae83bc5d1b6ab047ee24219424536
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-06 16:39:04 -0300

    feat: create product controller

---
+++ src/controllers/CreateProductsController.ts
@@ -0,0 +1,20 @@
+import { Request, response, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Category } from '../entities/Category';
+import { CreateProductsService } from '../services/CreateProductsService';
+
+export class CreateProductsController {
+  async handle(request: Request, response: Response) {
+    const { name, price, category_name } = request.body;
+    const categoryRepo = getRepository(Category);
+    try {
+      const category = await categoryRepo.findOne({ name: category_name });
+      const createProductsService = new CreateProductsService()
+      const product = await createProductsService.execute({ name, price, category });
+      return response.json(product)
+    } catch (error) {
+      return response.json({ error: error.message })
+    }
+
+  }
+}
