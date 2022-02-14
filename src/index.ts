12cf92592c8e8ac2cdeb1a3e7f0db6f76fce6177
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-14 09:39:01 -0300

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'reflect-metadata';
 import './database';
 import { routes } from './routes';
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads/', express.static(resolve(__dirname, '..', 'uploads')))
-
+app.use(cors());
 app.use(routes);
 
 app.listen(8000, () => console.log('Running on port 8000'));
